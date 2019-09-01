f60a3db37c94df76796337cadc9c32fef4899e94
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-09-01 11:24:47 -0400

    Refactoring of tests to the new core common resource api.

--- demo.tests/src/org/nasdanika/codegen/demo/tests/CodegenDemoTests.java
+++ demo.tests/src/org/nasdanika/codegen/demo/tests/CodegenDemoTests.java
@@ -11,6 +11,7 @@ import org.nasdanika.codegen.util.ValidatingModelGenerator;
 import org.nasdanika.common.MutableContext;
 import org.nasdanika.common.ProgressEntry;
 import org.nasdanika.common.SimpleMutableContext;
+import org.nasdanika.common.resources.BinaryEntityContainer;
 import org.nasdanika.common.resources.Container;
 import org.nasdanika.common.resources.FileSystemContainer;
 
@@ -31,9 +32,9 @@ public class CodegenDemoTests {
 	public void testHtmlSpringBootGeneration() throws Exception {
 		try {
 			ValidatingModelGenerator<String> validatingModelGenerator = new ValidatingModelGenerator<>(TEST_MODELS_BASE_URI+"html-spring-boot.codegen");
-			Container<InputStream> fsc = new FileSystemContainer(new File("target/test-output"));
+			BinaryEntityContainer fsc = new FileSystemContainer(new File("target/test-output"));
 			MutableContext mc = new SimpleMutableContext();
-			mc.register(Container.class, fsc);
+			mc.register(BinaryEntityContainer.class, fsc);
 			mc.put("name", "World");
 			
 			ProgressEntry pe = new ProgressEntry("Generating basic text", 0);		
